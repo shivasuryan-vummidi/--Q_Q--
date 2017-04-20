4541ea96dc5c49401153be2f9e9dcb2f349fa939
clivejj <cjohnston1@stuy.edu>
2017-04-20 11:45:55 -0400

    Add interface

---
+++ PriorityQueue.java
@@ -0,0 +1,15 @@
+public interface PriorityQueue <T> {
+
+    //Adds an item to this priority queue.
+    public void add(T x);
+
+    //Returns true if this stack is empty, otherwise returns false.
+    public boolean isEmpty();
+
+    //Returns the smallest item stored in this priority queue without removing it.
+    public T peekMin();
+
+    // Removes and returns the smallest item stored in this priority queue.
+    public T removeMin();
+
+}
